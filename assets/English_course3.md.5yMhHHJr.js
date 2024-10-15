fa27db03dd8fb1923ac59f8dcc185b1aa55d7512
mhpei <mhpei@gaojihealth.com>
2024-10-15 16:29:01 +0800

    add

---
+++ assets/English_course3.md.5yMhHHJr.js
@@ -0,0 +1,13 @@
+import{_ as s,c as n,o as a,V as e}from"./chunks/framework.DIPPIwJv.js";const g=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"English/course3.md","filePath":"English/course3.md"}'),p={name:"English/course3.md"},o=e(`<h3 id="如何巧妙礼貌地结束对话-how-to-end-a-conversation-skilfully" tabindex="-1">如何巧妙礼貌地结束对话 how to end a conversation skilfully? <a class="header-anchor" href="#如何巧妙礼貌地结束对话-how-to-end-a-conversation-skilfully" aria-label="Permalink to &quot;如何巧妙礼貌地结束对话 how to end a conversation skilfully?&quot;">​</a></h3><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span>did you see the wedding?</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>congratulations!</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>congratulations on you weddding! 新婚快乐！</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>goodbye</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>see you later 回头见</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>see you this weekend!</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>see you around 回头见</span></span></code></pre></div>`,2),l=[o];function t(i,c,r,d,u,h){return a(),n("div",null,l)}const f=s(p,[["render",t]]);export{g as __pageData,f as default};
